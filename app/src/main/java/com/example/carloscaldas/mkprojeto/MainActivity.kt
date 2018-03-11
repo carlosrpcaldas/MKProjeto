8289617d796b538a442ec7765984166119df4d59
adrianopogianeli <adriano.pogianeli@gmail.com>
2018-03-10 22:59:57 -0300

    add rebimento de valor do xml

--- app/src/main/java/com/example/carloscaldas/mkprojeto/MainActivity.kt
+++ app/src/main/java/com/example/carloscaldas/mkprojeto/MainActivity.kt
@@ -2,16 +2,33 @@ package com.example.carloscaldas.mkprojeto
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+
 
 class MainActivity : AppCompatActivity() {
 
-    override fun onCreate(icicle: Bundle?) {
-        super.onCreate(icicle)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        onClick(R.id.btLogin) {onClickLogin()}
-    }
-    fun onClickLogin(){
-        val login = getTextString(R.id.etUser)
+
+        btLogin.setOnClickListener {
+            onClickLogin()
+        }
+
+        //Toast.makeText(this, this.toString(), Toast.LENGTH_LONG).show()
 
     }
+
+     fun onClickLogin() {
+
+        val textView: TextView = findViewById<TextView>(R.id.etUser) as TextView
+        val login : String = textView.text.toString()
+          Toast.makeText(applicationContext,"Bem vindo $login", Toast.LENGTH_LONG).show()
+    }
+
+
+
 }
